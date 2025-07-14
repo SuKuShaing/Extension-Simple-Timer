d2aa9e0ca2d76bb11ecd5ce278d47108e281a736
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-14 12:27:55 -0400

    Aplicación funcional

--- background.js
+++ background.js
@@ -4,9 +4,15 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "show_alert" && message.tabId) {
-        chrome.scripting.executeScript({
-            target: { tabId: message.tabId },
-            func: () => alert("¡Hola desde tu extensión!")
+        chrome.tabs.get(message.tabId, (tab) => {
+            if (tab.url && (tab.url.startsWith('http') || tab.url.startsWith('file'))) {
+                chrome.scripting.executeScript({
+                    target: { tabId: message.tabId },
+                    func: () => alert("¡Hola desde tu extensión!")
+                });
+            } else {
+                console.log("No se puede ejecutar el script en esta URL:", tab.url);
+            }
         });
     }
 });
